5f667a1eb8aa83e5c16adc08d4aa2e37d3b89e77
Maimounadev <maimounatraoredev@gmail.com>
2023-04-30 22:09:32 -0400

    full auth shoe store app

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://maimounatraoredev:Enai123@cluster0.ikhc6zs.mongodb.net/store?retryWrites=true&w=majority', 
+    'dbName': 'store'
+};
